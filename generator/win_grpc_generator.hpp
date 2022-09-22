9682bc94e1cdec6aa88709e8df1e89ab592fffdd
Oscar Zhang <oscar.zth1999@gmail.com>
2022-09-22 10:42:22 -0700

    stage

--- generator/win_grpc_generator.hpp
+++ generator/win_grpc_generator.hpp
@@ -5,8 +5,16 @@
 
 namespace win_grpc_generator {
 
+// Return the prologue of the generated header file.
 grpc::string GetHeaderPrologue(grpc_generator::File* file);
 
+// Return the prologue of the generated source file.
+grpc::string GetSourcePrologue(grpc_generator::File* file);
+
+// Return the services for generated header file.
 grpc::string GetHeaderServices(grpc_generator::File* file);
+
+// Return the services for generated source file.
+grpc::string GetSourceServices(grpc_generator::File* file);
 
 } // win_grpc_generator
